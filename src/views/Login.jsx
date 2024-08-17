3a70c376e6a6775a11e0cd1e5c86fb6fca243d1e
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-17 16:46:49 +0430

    Create the login form

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
-  return <div>Login</div>;
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Login in to your account</h1>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button className="btn btn-block">Login</button>
+          <p className="message">
+            Not registered? <Link to="/register">Create an account</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
