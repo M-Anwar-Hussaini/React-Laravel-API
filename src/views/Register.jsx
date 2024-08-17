2462a60c00855ca3c4e9f5ab22f5b24652cb3ced
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-17 16:47:05 +0430

    Create the register page

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,3 +1,61 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
+
 export default function Register() {
-  return <div>Register</div>;
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+  const { setUser, setToken, user, token } = useStateContext();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    };
+    setToken("Here is the new token");
+    setUser({
+      name: "THe logged in user",
+      email: "the email",
+    });
+
+    axiosClient
+      .post("/register", payload)
+      .then(({ data }) => {
+        setToken(data.token);
+        setUser(data.user);
+      })
+      .catch((error) => {
+        const response = error.response;
+        if (response && response.status === 422) {
+          console.log(response.data.errors);
+        }
+      });
+  };
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Signup for free</h1>
+          <input ref={nameRef} type="text" placeholder="Full Name" />
+          <input ref={emailRef} type="email" placeholder="Email Address" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
+          <input
+            ref={passwordConfirmationRef}
+            type="password"
+            placeholder="Password Confirmation"
+          />
+          <button className="btn btn-block">Signup</button>
+          <p className="message">
+            Already registered? <Link to="/login">Login to your account</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
