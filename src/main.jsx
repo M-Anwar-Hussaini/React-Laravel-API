d0ff8d0ec82473c6fb7ca2eef07b59d7801cdef6
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-17 16:45:28 +0430

    Wrap the application inside the ContextProvider element

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
+import "./index.css";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
+import { ContextProvider } from "./contexts/ContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />,
+  <ContextProvider>
+    <RouterProvider router={router} />,
+  </ContextProvider>,
 );
