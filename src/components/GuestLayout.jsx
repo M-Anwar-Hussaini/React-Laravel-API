d0105b41c26540c4f05b0035b5f9a3dedcc70079
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-17 16:46:20 +0430

    Check if there is a token while using the GuestLayout

--- src/components/GuestLayout.jsx
+++ src/components/GuestLayout.jsx
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function GuestLayout() {
+  const { token } = useStateContext();
+  if (token) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
-      For guest layout only
       <Outlet />
     </div>
   );
