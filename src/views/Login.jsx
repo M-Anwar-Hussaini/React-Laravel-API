c25f9be2a73e75d1b3d8eb2cffc969bbc31b055d
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-18 19:35:58 +0430

    Implement the login functionality

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,16 +1,55 @@
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Login() {
-  const onSubmit = (e) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { setUser, setToken } = useStateContext();
+  const [errors, setErrors] = useState(null);
+
+  const onSubmit = async (e) => {
     e.preventDefault();
+    const payload = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+    setErrors(null);
+    try {
+      const res = await axiosClient.post("/login", payload);
+      const { token, user } = res.data;
+      setToken(token);
+      setUser(user);
+    } catch (error) {
+      console.log(error);
+      const response = error.response;
+      if (response && response.status === 422) {
+        if (response.data.errors) {
+          setErrors(response.data.errors);
+        } else {
+          setErrors({
+            email: [response.data.message],
+          });
+        }
+      }
+      console.log(errors);
+    }
   };
   return (
     <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Login in to your account</h1>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          {errors && (
+            <div className="alert">
+              {Object.keys(errors).map((key) => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          )}
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
           <button className="btn btn-block">Login</button>
           <p className="message">
             Not registered? <Link to="/register">Create an account</Link>
