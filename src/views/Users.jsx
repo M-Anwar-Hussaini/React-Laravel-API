080ec0407856ac42d21325c263e91055d6c1d56c
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-19 12:53:13 +0430

    Implement the Users component

--- src/views/Users.jsx
+++ src/views/Users.jsx
@@ -1,3 +1,105 @@
+import { useEffect, useState } from "react";
+import axiosClient from "../axios-client";
+import { Link } from "react-router-dom";
+
 export default function Users() {
-  return <div>Users</div>;
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    setLoading(true);
+    try {
+      const res = await axiosClient.get("/users");
+      setUsers(res.data.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async (user) => {
+    try {
+      const confirm = window.confirm("Are you sure to delete the user?");
+      if (!confirm) {
+        return;
+      }
+      await axiosClient.delete(`/users/${user.id}`);
+      getUsers();
+      // TODO: show the notification
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h1>Users</h1>
+        <Link className="btn-add" to="/users/new">
+          Create new user
+        </Link>
+      </div>
+      <div className="card animated fadeInDown">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Create Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          {loading ? (
+            <>
+              <tbody>
+                <tr>
+                  <th colSpan={5} className="text-center">
+                    Loading
+                  </th>
+                </tr>
+              </tbody>
+            </>
+          ) : (
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.created_at}</td>
+                  <td
+                    style={{
+                      display: "flex",
+                      gap: "10px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Link className="btn-edit" to={`/users/${user.id}/edit`}>
+                      Edit
+                    </Link>
+                    <button
+                      onClick={(e) => onDelete(user)}
+                      className="btn-delete"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
+    </div>
+  );
 }
