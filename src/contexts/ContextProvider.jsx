11e5cba45216f025724655a3b4a46aa03ee8c612
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-23 21:12:22 +0430

    add a new state for the contextProvider

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -3,14 +3,17 @@ import { createContext, useContext, useState } from "react";
 const StateContext = createContext({
   user: null,
   token: "",
+  notification: null,
   setUser: () => {},
   setToken: () => {},
+  setNotification: () => {},
 });
 
 // eslint-disable-next-line react/prop-types
 export const ContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+  const [notification, _setNotification] = useState("");
   const setToken = (token) => {
     _setToken(token);
     if (token) {
@@ -19,8 +22,16 @@ export const ContextProvider = ({ children }) => {
       localStorage.removeItem("ACCESS_TOKEN");
     }
   };
+  const setNotification = (message) => {
+    _setNotification(message);
+    setTimeout(() => {
+      _setNotification("");
+    }, 5000);
+  };
   return (
-    <StateContext.Provider value={{ user, token, setUser, setToken }}>
+    <StateContext.Provider
+      value={{ user, token, setUser, setToken, notification, setNotification }}
+    >
       {children}
     </StateContext.Provider>
   );
